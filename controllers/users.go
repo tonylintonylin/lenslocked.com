6de712b57939561709c0157489995ab16814aa17
Tony Lin <tonylinbusiness@gmail.com>
2021-01-01 21:33:42 -0500

    added parseForm helper

--- controllers/users.go
+++ controllers/users.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/schema"
-
 	"lenslocked.com/views"
 )
 
@@ -41,17 +39,10 @@ type SignupForm struct {
 // CREATE
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		panic(err)
-	}
 
-	dec := schema.NewDecoder()
 	var form SignupForm
-	if err := dec.Decode(&form, r.PostForm); err != nil {
+	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
 	fmt.Fprintln(w, form)
-	// r.PostForm = map[string][]string
-	// fmt.Fprintln(w, r.PostForm["email"])
-	// fmt.Fprintln(w, r.PostForm["password"])
 }
