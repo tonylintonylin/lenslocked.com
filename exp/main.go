215bb425977759da3b1c3593ea29d446e8e3df73
Tony Lin <tonylinbusiness@gmail.com>
2021-01-02 13:50:05 -0500

    connecting to postgres

---
+++ exp/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = ""
+	dbname   = "lenslocked_dev"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("sucessfuly connected")
+	db.Close()
+}
