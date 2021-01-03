100fc1aa20689e494d2f6c6b76c9093d4cc32c28
Tony Lin <tonylinbusiness@gmail.com>
2021-01-03 16:27:33 -0500

    creating cookies on login and signup, added redirect

--- controllers/users.go
+++ controllers/users.go
@@ -61,7 +61,9 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintln(w, user)
+	signIn(w, &user)
+
+	http.Redirect(w, r, "/cookietest", http.StatusFound)
 }
 
 type LoginForm struct {
@@ -92,12 +94,18 @@ func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	signIn(w, user)
+
+	http.Redirect(w, r, "/cookietest", http.StatusFound)
+}
+
+func signIn(w http.ResponseWriter, user *models.User) {
 	cookie := http.Cookie{
 		Name:  "email",
 		Value: user.Email,
 	}
 	http.SetCookie(w, &cookie)
-	fmt.Fprintln(w, user)
+	return
 }
 
 // CookieTest is used to display cookies set on the current user
