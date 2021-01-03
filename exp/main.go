4745c5dcc33b4d8ad9f82d915613bf16023142ad
Tony Lin <tonylinbusiness@gmail.com>
2021-01-03 18:04:30 -0500

    created the rand pacakage that helps create strings and random remember tokens

--- exp/main.go
+++ exp/main.go
@@ -3,50 +3,10 @@ package main
 import (
 	"fmt"
 
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"lenslocked.com/models"
-)
-
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = ""
-	dbname   = "lenslocked_dev"
+	"lenslocked.com/rand"
 )
 
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	us, err := models.NewUserService(psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer us.Close()
-
-	user := models.User{
-		Name:  "Mike",
-		Email: "mike@example.com",
-	}
-
-	if err := us.Create(&user); err != nil {
-		panic(err)
-	}
-
-	user.Email = "mike@anothercompany.com"
-	if err := us.Update(&user); err != nil {
-		panic(err)
-	}
-
-	userByEmail, err := us.ByEmail("mike@anothercompany.com")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(userByEmail)
-
-	userByID, err := us.ByID(user.ID)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(userByID)
+	fmt.Println(rand.String(10))
+	fmt.Println(rand.RememberToken)
 }
